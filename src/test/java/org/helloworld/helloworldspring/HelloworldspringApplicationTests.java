56deaf44737bc1111df431ca20fed0b9e7eb798d
eel <unagiman@icloud.com>
2022-11-30 07:39:52 -0700

    First Commit

---
+++ src/test/java/org/helloworld/helloworldspring/HelloworldspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helloworld.helloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
